6cbc3cbe2717fdf3e1ff11cf0a136828548ad1cb
Mefgalm <mefgalm@gmail.com>
2019-04-10 22:52:52 +0300

    disable textarea

--- src/View.fs
+++ src/View.fs
@@ -86,6 +86,7 @@ let outputBlock model =
     div [ ClassName "output-area" ]
         [ textarea [ ClassName "output-text-area"
                      ReadOnly true
+                     Disabled true
                      Style [ Color (getColor (model.Output)) ]
                      Value (getText model.Output) ] [] ]
 
